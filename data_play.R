7355be42ec2abf525beece070eacd97dc317f6f9
Adela Iliescu <adela.iliescu@ons.gov.uk>
2022-11-17 15:36:42 +0000

    Made the plot more generic

--- data_play.R
+++ data_play.R
@@ -3,12 +3,13 @@ library(ggplot2)
 data_arrests = as.data.frame(get(data('USArrests')))
 data_arrests <- cbind(location = rownames(data_arrests), data_arrests)
 
-murder = ggplot(data_arrests, aes(
-                x = Murder, 
-                y= location))+
+selected_x = 'Assault'
+
+sel_plot = ggplot(data_arrests, aes(
+                x = data_arrests[, selected_x], 
+                y= rownames(data_arrests)))+
                 geom_bar(stat='identity')+
+                ylab("Location")
                 ggthemes::theme_tufte()
-
-
 
 
